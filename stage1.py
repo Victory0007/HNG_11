41d854a4cb1470a2cf6f6a03d9cca2cd35554d7f
Ezenwa Victory <ezenwavictory744@gmail.com>
2024-07-02 10:42:18 +0100

    first push stage1

---
+++ stage1.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    print(request)
+    visitor_name = request.args.get('visitor_name')
+    client_ip = request.remote_addr
+
+    # location data and temperature for illustration purposes
+    location = "New York"
+    temperature = 11
+
+    response = {
+        "client_ip": client_ip,
+        "location": location,
+        "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
