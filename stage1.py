cf879ff4804a4fdacc7a47edbe151cde1ed25bf1
Ezenwa Victory <ezenwavictory744@gmail.com>
2024-07-02 12:11:01 +0100

    fixing weather app stage1

--- stage1.py
+++ stage1.py
@@ -1,4 +1,11 @@
 from flask import Flask, request, jsonify
+from dotenv import find_dotenv, load_dotenv
+import os
+import requests
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+weather_app_api_key = os.getenv("WEATHER_APP_API_KEY")
 
 app = Flask(__name__)
 
@@ -6,19 +13,43 @@ app = Flask(__name__)
 def home():
     return "<h1> Home Page </h1>"
 
+# @app.route('/api/hello', methods=['GET'])
+# def hello():
+#     visitor_name = request.args.get('visitor_name')
+#     client_ip = request.remote_addr
+#
+#     # location data and temperature for illustration purposes
+#     location = "New York"
+#     temperature = 11
+#
+#     response = {
+#         "client_ip": client_ip,
+#         "location": location,
+#         "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+#     }
+#
+#     return jsonify(response)
+
 @app.route('/api/hello', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name')
     client_ip = request.remote_addr
 
-    # location data and temperature for illustration purposes
-    location = "New York"
-    temperature = 11
+    # Get location based on IP
+    location_response = requests.get(f'https://ipinfo.io/{client_ip}/json')
+    location_data = location_response.json()
+    city = location_data.get('city', 'Unknown Location')
 
+    # Get temperature based on location (dummy API key)
+    weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={weather_app_api_key}')
+    weather_data = weather_response.json()
+    temperature = weather_data['main']['temp']
+
+    # Build response
     response = {
         "client_ip": client_ip,
-        "location": location,
-        "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+        "location": city,
+        "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
     }
 
     return jsonify(response)
